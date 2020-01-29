15e3e503992cf7e8e87b536b34cd7b3682aaa04b
yummy2108 <949657511@qq.com>
2020-01-29 19:44:51 +0800

    Add files via upload

---
+++ Train_FER/front_end.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import math
+
+class VGG(nn.Module):
+    def __init__(self, features):
+        super(VGG, self).__init__()
+        self.features = features
+
+    def forward(self, x):
+        x = self.features(x)
+        return x
+
+
+
+def make_layers(cfg, batch_norm=False):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
+
+cfg = {
+    'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'B': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512],
+    'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+def fornt_end():
+    vgg16 = torchvision.models.vgg16(pretrained=True)
+    model = VGG(make_layers(cfg['D']))
+    
+    # 读取参数
+    
+    pretrained_dict = vgg16.state_dict()
+    model_dict = model.state_dict()
+    # 将pretrained_dict里不属于model_dict的键剔除掉
+    pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
+    # 更新现有的model_dict
+    model_dict.update(pretrained_dict)
+    # 加载我们真正需要的state_dict
+    model.load_state_dict(model_dict)
+    
+    return model
